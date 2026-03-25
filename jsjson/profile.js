051db13051ba84aceb5a8a56ad2d0ff65ba3d469
Prokopenko, Tymur - Student RIjn IJssel <00332668@rijnijssel.nl>
2026-03-25 19:20:00 +0100

    Created info in profile

--- jsjson/profile.js
+++ jsjson/profile.js
@@ -39,13 +39,12 @@ fetch('/profile')
                 .then(user => {
                     document.getElementById('user-name').textContent = user.name;
                     document.getElementById('user-email').textContent = user.email;
+
                     console.log('User from server:', user);
                     document.getElementById('user-admin').textContent = user.isAdmin ? 'true' : 'false';
-                    document.getElementById('user-paid').textContent = user.isPaid ? 'true' : 'false';
+                    document.getElementById('user-paid').textContent = user.isPaid ? document.getElementById('user-created').textContent = user.createdAt : 'false';
 
-                    // Пример условного отображения контента
                     if (user.isPaid) {
-                    // показываем дополнительную информацию
                     document.getElementById('premium-content').style.display = 'block';
                     } else {
                     document.getElementById('premium-content').style.display = 'none';
@@ -58,31 +57,5 @@ fetch('/profile')
         fetch('/logout')
             .then(() => window.location.href = '/');
         }
-
-
-
-
-
-
-
-// animation
-const animatedElements = document.querySelectorAll('.fade-in-element');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show'); // Сбрасываем анимацию
-        }
-    });
-}, { threshold: 0.4 });
-
-animatedElements.forEach(el => observer.observe(el));
-
-
-// function anotherPage() {
-//       window.location.href = "log-in.html"; 
-// }
 
         
