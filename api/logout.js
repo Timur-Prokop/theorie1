daaef4d3ba053912cf34978adf43e806cec871db
Prokopenko, Tymur - Student RIjn IJssel <goodtime2804@gmail.com>
2026-08-15 19:41:59 +0200

    deleted auth/payment

--- api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require("./_lib");
-
-module.exports = async function handler(req, res) {
-  clearSessionCookie(res);
-  return res.status(200).json({ success: true });
-};
